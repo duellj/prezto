4b312d5f62031392ddcbb911f66b7ce11f77da95
Jon Duell <jon@jonduell.com>
2014-01-17 16:34:14 -0800

    Updates php switchers

--- modules/php/init.zsh
+++
@@ -1,9 +0,0 @@
-########################################################################################
-# php-version (activate default PHP version and autocompletion)
-# PHP_HOME                      => should reflect location of compiled PHP versions
-# PHPVERSION_DISABLE_COMPLETE=1 => to disable shell completion
-########################################################################################
-export PHPVERSIONDISABLE_COMPLETE="1"
-export PHP_VERSIONS="/usr/local/Cellar/"
-
-[ -f $(brew --prefix php-version)/php-version.sh ] && source $(brew --prefix php-version)/php-version.sh && php-version php53/5.3.14 > /dev/null
